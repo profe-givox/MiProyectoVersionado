04385ac1d2c5791e97288155d8d4dde6450621af
Gustavo Ivan Vega Olvera <isc.givox@gmail.com>
2022-09-06 11:40:53 -0500

    Se agregaron un layout y ub boton

--- app/src/main/java/net/ivanvega/miproyectoversionado/MainActivity.kt
+++ app/src/main/java/net/ivanvega/miproyectoversionado/MainActivity.kt
@@ -2,8 +2,12 @@ package net.ivanvega.miproyectoversionado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var lbl : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
